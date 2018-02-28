ca3e53ec8d88421a0e880b66d1981b703c27b71c
DaviVolcan <davi.volcan@me.com>
2018-02-28 20:05:39 -0300

    Update funçao.c

--- funçao.c
+++ funçao.c
@@ -1,7 +1,12 @@
 #include <"fonte.h">
 //tentativa de escrever o numero dois numa variavel e ens seguida jogar ela no display 
-int resultado 
+int resultado [7][9];
 
+void print(int caracter)
+{
+
+
+}
 
 
 
